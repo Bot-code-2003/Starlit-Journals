e4c909af55bc255f405d8ddd7025136b30f04dc0
Dharmadeep <dharmadeepmadisetty@gmail.com>
2025-03-25 11:40:01 +0530

    Upper case enum error fixed

--- client/src/Components/ProfileSettings.jsx
+++ client/src/Components/ProfileSettings.jsx
@@ -578,9 +578,9 @@ const ProfileSettings = () => {
                         } border focus:outline-none`}
                       >
                         <option value="">Select Gender</option>
-                        <option value="Male">Male</option>
-                        <option value="Female">Female</option>
-                        <option value="Other">Other</option>
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
+                        {/* <option value="Other">Other</option> */}
                       </select>
                     </div>
 
